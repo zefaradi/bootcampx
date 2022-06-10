db583ddf47daa869a47a7832913c7bf0d8187e47
Adib Zefar <adibzefar@gmail.com>
2022-06-10 16:22:44 -0400

    add 1_queries

--- 1_queries/1_Students_in_Cohort.sql
+++ 1_queries/1_Students_in_Cohort.sql
@@ -2,3 +2,7 @@ SELECT id, name
 FROM students
 WHERE cohort_id = 1
 ORDER BY name;
+
+SELECT students.name as student_name, cohorts.name as cohort_name, cohorts.start_date as cohort_start_date, students.start_date as student_start_date
+FROM cohorts JOIN students ON cohort_id = cohorts.id
+WHERE cohort_start_date <> student_start_date;
