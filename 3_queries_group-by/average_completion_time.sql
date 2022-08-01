9e1e684306c7def0f3a1c9daa710700eb8b820e7
Adib Zefar <adibzefar@gmail.com>
2022-08-01 01:01:59 -0400

    updated the code

--- 3_queries_group-by/average_completion_time.sql
+++ 3_queries_group-by/average_completion_time.sql
@@ -1,6 +1,6 @@
 SELECT students.name as student, AVG(assignment_submissions.duration) AS average_assignment_duration
 FROM students
-JOIN assignment_submissions ON students.id = assignment_submissions.id
+JOIN assignment_submissions ON students.id = student_id
 WHERE students.end_date is NULL
 GROUP BY student
 ORDER BY AVG(assignment_submissions.duration) DESC;
