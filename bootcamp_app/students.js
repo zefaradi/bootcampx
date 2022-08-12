bea79637ebff797c4710d6664883c8d3934d3ba3
Adib Zefar <adibzefar@gmail.com>
2022-08-11 23:07:59 -0400

    refactor code to prevent SQL injections

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,12 +7,21 @@ const pool = new Pool({
   database: 'labber'
 });
 
-pool.query(`
-SELECT id, name, cohort_id
+const queryString = `
+SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
-LIMIT 5;
-`)
-.then(res => {
-  console.log(res.rows);
-})
-.catch(err => console.error('query error', err.stack));
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`;
+
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`, limit];
+
+pool.query(queryString, values).then(res => {
+  res.rows.forEach(user => {
+    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+  })
+});
--- bootcamp_app/teacher.js
+++ bootcamp_app/teacher.js
@@ -9,16 +9,20 @@ const pool = new Pool({
 
 console.log("connected");
 
-pool.query(`SELECT teachers.name as teacher, cohorts.name as cohort, count(assistance_requests.*) as total_assistances
+const queryString = `SELECT teachers.name as teacher, cohorts.name as cohort, count(assistance_requests.*) as total_assistances
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2]}'
+WHERE cohorts.name LIKE $1
 GROUP BY teachers.name, cohorts.name
 ORDER BY teachers.name;
-`)
-.then(res => {
+`;
+
+const cohortName = process.argv[2];
+const values = [`%${cohortName}%`];
+
+pool.query(queryString, values).then(res => {
   res.rows.forEach(item => {
     console.log(`${item.cohort}: ${item.teacher}`);
   })
